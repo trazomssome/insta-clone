bc289773c5efa95c60ecc9815ed14386eefad56e
trazomssome <trazomssome@gmail.com>
2021-07-22 12:49:23 +0900

    3.5

--- notes.md
+++ notes.md
@@ -5,3 +5,26 @@
 1. 이전 강의는 graphql-yoga를 이용하였으나 apollo server가 충분히 간단해졌으므로 사용하지 않음.
 2. npm i apollo-server graphql
 3. 서버 세팅 진행 https://github.com/apollographql/apollo-server
+
+## 3.2 babel
+
+1. 최신 javascript 사용을 위해 babel 설치
+
+## 3.3 POC API
+
+1. Proof of Concept용 영화 API 만들어보기
+
+## 3.4 Prisma Setup
+
+1. Prisma는 ORM(Object Relational Mapper) 객체와 관계형 데이터베이스의 데이터를 자동으로 매핑해주는 것)
+2. schema.prisma파일의 datasource는 데이터베이스의 주소(url)와 종류(provider)를 알려주는 역할
+
+## 3.5 Prisma Migrate
+
+1. dev flag로 실행하면 client를 자동으로 만들어줌(설정필요없음)
+2. client는 데이터베이스와 어떻게 소통하는지에 대한 설정
+3. migration 할때 윈도우 환경의 경우 .env의 database_url 입력 시 비밀번호까지 입력해줘야함
+
+## 3.6 Prisma Client
+
+1. client는 node_module내부에 위치함
--- server.js
+++ server.js
@@ -1,17 +1,47 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient();
 
 // The GraphQL schema
 const typeDefs = gql`
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
+  }
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => client.movie.findMany(),
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
